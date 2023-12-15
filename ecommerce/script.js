5f2498db2e4bb19972ddc81f70c50384afa666c7
adershradhakrishnan <adershradhakrishnan07@gmail.com>
2023-12-15 20:08:31 +0530

    fdh message

--- ecommerce/script.js
+++ ecommerce/script.js
@@ -1,4 +1,63 @@
-{
-    let xhr = new XMLHttpRequest();
-    xhr.open("get",'')
+
+
+let xhr = new XMLHttpRequest();
+
+let btn = document.getElementById('btn');
+btn.addEventListener('click',() =>{
+  xhr.send();
+});
+//get
+//post
+//put
+//delete
+
+xhr.open("get",'https://fakestoreapi.com/products');
+
+
+console.log("xhr: ",xhr);
+
+xhr.onreadystatechange = function() {
+  console.log("readystate: ",xhr.readyState);
+  console.log("statuscode:",xhr.status);
+
+  let content =document.getElementById('content');
+
+  if(xhr.readyState == 4) {
+    
+    if(xhr.status == 200) {
+
+      let response = xhr.response;
+      console.log("response: ",response);
+      console.log("type of response : ",typeof(response));
+
+      let parsed_response = JSON.parse(response);
+      console.log("parsed_response: ",parsed_response);
+      console.log("type of parsed_response: ",typeof(parsed_response));
+
+      datas = '';
+
+      for(let i=0; i<parsed_response.length;i++) {
+        datas = datas + `
+        <tr>
+        <td>${parsed_response[i].id}</td>
+        <td>${parsed_response[i].name}</td>
+        <td>${parsed_response[i].username}</td>
+        <td>${parsed_response[i].price}</td>
+        </tr>
+        `
+      }
+       content.innerHTML = datas;
+
+    }else {
+      console.log("Failed");
+    }
+
+  }else {
+    console.log("State not completed");
+  }
+
+  
 }
+
+
+
