6eab41d28df6e2c82964231ce4d200bfe86a9238
adershradhakrishnan <adershradhakrishnan07@gmail.com>
2023-12-28 09:54:57 +0530

    hghfydt message

--- server1/app.js
+++ server1/app.js
@@ -2,20 +2,57 @@ const http = require('http');
 const url = require('url');
 const port = 3000;
 const fs = require('fs');
+const queryString = require('querystring');
 
 
 const server = http.createServer((req,res) => {
+
     console.log("url: ",req.url);
+
+
+    //parse the request url
     const parsed_url = url.parse(req.url);
     console.log("parsed_url: ",parsed_url);
+
+
+    //serve the html file on root request
 
     if(parsed_url.pathname === '/') {
       res.writeHead(200,{'Content-Type' : 'text/html'});
       res.end(fs.readFileSync('../client/index.html'));
     }
 
-    if(req.method === "POST" && parsed_url.pathname === "/submit");
+
+    //handle form submission on post request to/submit
+
+    if(req.method === "POST" && parsed_url.pathname === "/submit") {
     console.log("form submitted succesfully..");
+    };
+
+
+    let body='';
+
+    //collect data as it comes in chunks
+
+    req.on('data',(chunk)=> {
+      console.log("chunk: ",chunk);
+      console.log('chunk.toString() :',chunk.toString());
+      body+=chunk.toString();
+      console.log("body: ",body);
+    });
+
+    //process the form data on end of the request
+    req.on('end',()=> {
+      const formData = queryString.parse(body);
+      console.log("formData: ",formData);
+
+      //do something with the submitted data
+      console.log("name: ",formData.name);
+      console.log("email: ",formData.email);
+      console.log("password: ",formData.password);
+    });
+    
+      //save to a database(next)
 
     res.writeHead(200,{'Content-Type' : 'text/plain'});
     res.end("Form submitted succesfully");
@@ -25,3 +62,5 @@ const server = http.createServer((req,res) => {
   server.listen(port,()=> {
     console.log(`Server running at http://127.0.0.1:${port}`)
   });
+
+
