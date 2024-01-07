fa17d490eb9c575470ce3c7ded318f34fc57d659
adershradhakrishnan <adershradhakrishnan07@gmail.com>
2024-01-07 20:50:33 +0530

    dsssssssf message

--- client/script.js
+++ client/script.js
@@ -15,9 +15,9 @@ async function geuserData() {
         content = content + `
         <tr>
         <td>${parsedUserData[i]._id}</td>
-        <td><input type="text" name="name" id="name-${parsedUserData[i]._id}" value=${parsedUserData[i].name} disabled="true"</td>
-        <td><input type="email" name="email" id="email-${parsedUserData[i]._id}" value=${parsedUserData[i].email} disabled="true"</td>
-        <td><input type="password" name="password" id="password-${parsedUserData[i]._id}" value=${parsedUserData[i].password} disabled="true"</td>
+        <td><input type="text" name="name" id="name-$${parsedUserData[i]._id}" value=${parsedUserData[i].name} disabled="true"</td>
+        <td><input type="email" name="email" id="email-$${parsedUserData[i]._id}" value=${parsedUserData[i].email} disabled="true"</td>
+        <td><input type="password" name="password" id="password-$${parsedUserData[i]._id}" value=${parsedUserData[i].password} disabled="true"</td>
         <td><button onclick="handleEdit('${parsedUserData[i]._id})">Edit</button></td>
         </tr>
         `
@@ -29,18 +29,18 @@ async function geuserData() {
 geuserData();
 
 function handleEdit(id) {
-    let id = id;
+    let _id = id;
     console.log("id: ",_id);
 
-    let name = document.getElementById('name-${_id}');
+    let name = document.getElementById(`name-${_id}`);
     console.log("name: ",name);
     name.disabled = false;
 
-    let email = document.getElementById('email-${_id}');
+    let email = document.getElementById(`email-${_id}`);
     console.log("email: ",email);
     email.disabled = false;
 
-    let password = document.getElementById('password-${_id}');
+    let password = document.getElementById(`password-${_id}`);
     console.log("password: ",password);
     password.disabled = false;
 
