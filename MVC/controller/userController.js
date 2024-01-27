23076adfaf5ef0ada31e0b476d2d938a5acaffc4
adershradhakrishnan <adershradhakrishnan07@gmail.com>
2024-01-27 15:58:02 +0530

    sfsd message

--- MVC/controller/userController.js
+++ MVC/controller/userController.js
@@ -1,25 +1,54 @@
 const users = require('../db/models/users');
+const success_function = require('../utils/response-handler').success_function;
+const error_function = require('../utils/response-handler').error_function;
 
-async function createUser(req,res) {
+ exports.createUser = async function(req,res) {
     try{
         const datas = req.body;
+        //validate
+        const isUserExist = await users.findOne({email : datas.email});
+        console.log("isUserExist: ",isUserExist);
+
+        if(isUserExist) {
+            let response = error_function({
+                statusCode : 400,
+                message : "User already exist",
+            });
+            res.status(response.statusCode).send(response);
+        }
         const new_user = await users.create(datas);
 
         if(new_user){
-            res.status(201).send("success");
+            let response = success_function({
+                statusCode : 201,
+                data : new_user,
+                message : "User created successfully",
+            })
+            res.status(response.statusCode).send(response);
             return;
         } else{
-            res.status(400).send('failed');
+            let response = error_function({
+                statusCode : 400,
+                message : "User creation failed",
+            })
+            res.status(response.statusCode).send(response);
             return;
         }
     } catch(error){
         console.log("error: ",error);
-        res.status(400).send("failed");
+        let response = error_function({
+            statusCode : 400,
+            message : error.message?error.message:error,
+        })
+        res.status(response.statusCode).send(response);
         return;
     }
 }
 
-async function getUserData(){
+ exports.getUserData =async function(req,res){
+    try{
+        cons
+    }
 
 }
 
@@ -30,9 +59,3 @@ async function deleteUser(){
 
 }
 
-module.exports = {
-    createUser,
-    getUserData,
-    updateUser,
-    deleteUser,
-}
--- MVC/routes/userRoutes.js
+++ MVC/routes/userRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controller/userController');
 
-router.post('/submit,userController.createUser');
+router.post('/submit',userController.createUser);
 
 module.exports = router;
