9f3dc05117d385a37ee8fb59ba26a7948dd80ad9
adershradhakrishan <adershradhakrishnan07@gmail.com>
2024-02-05 15:50:43 +0530

    sdseee message

---
+++ bookmyshow/bookclient/script.js
@@ -0,0 +1,9 @@
+async function submitForm(){
+    let title = document.getElementById('title').value;
+    console.log("title: ",title);
+    let actor = document.getElementById('actor').value;
+    console.log("actor: ",actor);
+    let actress = document.getElementById('actress').value;
+    console.log("actress: ",actress);
+
+}
