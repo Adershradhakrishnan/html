24b37eff94356ea37b420622864eb58cce4b4372
adershradhakrishnan <adershradhakrishnan07@gmail.com>
2024-02-19 10:57:18 +0530

    weawr message

--- bookmyshow/bookclient/script.js
+++ bookmyshow/bookclient/script.js
@@ -6,30 +6,48 @@ async function submitForm(){
     let director = document.getElementById('director').value;
     console.log("director: ",director);
 
-    let data = {
-        title,
-        actor,
-        director,
-    }
+    let imageInput = document.getElementById('image');
 
-    let json_data = JSON.stringify(data);
-    let response = await fetch('/submit',{
-        "method" : "POST",
-        "headers" : {
-            "Content-Type" : "application/json",
-        },
-        "body" : json_data,
-    });
+    if(imageInput.files && imageInput.files[0]){
+        const reader = new FileReader();
 
-    let parsed_response = await response.text();
+        reader.onload = async function (e) {
+            const base64Image = e.target.result;
+            console.log("base64Image: ",base64Image);
 
-    if(parsed_response === "success") {
-        alert("Form submitted succesfully");
-    }else {
-        alert("Form submission failed");
+            let data = {
+                title,
+                actor,
+                director,
+                base64Image,
+            }
+
+            let json_data = JSON.stringify(data);
+            try{
+                let response = await fetch('/submit',{
+                    "method" : "POST",
+                    "headers" : {
+                        "Content-Type" : "application/json",
+                    },
+                    "body" : json_data,
+                });
+                let parsed_response = await response.text();
+
+                if(parsed_response === "success") {
+                    alert("Form submitted succesfully");
+                }else {
+                    alert("Form submission failed");
+                }
+
+            } catch(error) {
+                console.log("Error: ",error);
+                alert("An error occured while submitting the form");
+            }
+            
+        }
+        reader.readAsDataURL(imageInput.files[0]);
     }
 }
-
 async function getFilmData() {
     let filmData = await fetch('/getFilms');
     console.log("filmData: ",filmData);
@@ -42,18 +60,50 @@ async function getFilmData() {
     for(let i=0;i<parsedFilmData.length;i++){
         content += `
         <div class="card-body>
-        <img src="../../bookmyshow/images/downloadvaliban.jpg>
-        <h5 class="card-title" name="title" id="title-${parsedFilmData[i]._id}" value="${parsedFilmData[i].title}">
-        <h6 class="card-actor" name="actor" id="actor-${parsedFilmData[i]._id}" value="${parsedFilmData[i].actor}">
-        <h6 class="card-director" name="director" id="director-${parsedFilmData[i]._id}" value="${parsedFilmData[i].director}">
+        <img src="${parsedFilmData[i].image}" alt="User Image" style="max-width:100px;max-height:100px;">
+        <h5 class="card-title" name="title" id="title-${parsedFilmData[i]._id}">${parsedFilmData[i].title}</h5>
+        <h6 class="card-actor" name="actor" id="actor-${parsedFilmData[i]._id}">${parsedFilmData[i].actor}</h6>
+        <h6 class="card-director" name="director" id="director-${parsedFilmData[i]._id}">${parsedFilmData[i].director}</h6>
+        <a href="details.html" onclick="detailsmovie('${parsedFilmData[i]._id}')">Details</a>
         <div>
         `;
     }
 
     filmcontainer.innerHTML = content;
+}
+
+    getFilmData();
+
+    async function detailsmovie(id) {
+
+        let _id = id;
+        console.log("_id: ",_id);
+
+        let title = document.getElementById(`title-${id}`).value;
+        console.log("title: ",title);
+    }
+
+
+
+
+   
+
+   
+    
+
+   
+    
+
+
+
+
+   
+
+    
+
+    
      
 
 
 
 
-}
--- bookmyshow/server.js
+++ bookmyshow/server.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const multer = require('multer');
+// const multer = require('multer');
 const dotenv = require('dotenv');
+const fs = require('fs');
 const path = require('path');
+const cors = require('cors');
 const error = require('mongoose/lib/error');
 dotenv.config();
 let port = process.env.PORT;
 
 app.use(express.static(__dirname + "/bookclient"));
-app.use(express.static(path.join(__dirname, 'image')));
 app.use('/images',express.static(path.join(__dirname, 'images')));
+app.use('/uploads',express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+app.use(express.text());
+app.use(cors());
 
 let schema = new mongoose.Schema({
     title : {
@@ -28,51 +32,72 @@ let schema = new mongoose.Schema({
         type : String,
         required : true,
     },
+    image: {
+        type : String,
+        required : true,
+    }
 });
 
 const model = mongoose.model("film",schema);
 
-const storage = multer.memoryStorage();
-const upload = multer({storage: storage});
-
-app.get('/test',(req,res)=>{
-    res.status(200).send("Test success");
-});
 
 app.post('/submit',async(req,res)=>{
-    let data = req.body;
-    console.log("data: ",data);
+    let {title,actor,director,base64Image} = req.body;
 
-    const isUserExist = await model.findOne({title:data.title});
+    if(!title || !actor || !director || !base64Image) {
+        return res.status(400).send("All fields are required");
+    }
+    
+
+    const isUserExist = await model.findOne({title:title});
     console.log("isUserExist: ",isUserExist);
 
     if(isUserExist){
-        res.status(400).send("user already exists");
-        return;
+       return res.status(400).send("user already exists");
+        
     }
 
-    const result = await model.insertOne({
-        title: req.body.title,
-        actor: req.body.actor,
-        director:req.body.director,
-        image: req.file.buffer.toString('base64')
-    })
+    const Image = base64Image.split(';base64').pop();
+    const binaryImage = Buffer.from(Image,'base64');
 
-    
-    .then((message)=>{
+    const uploadDir = path.join(__dirname,'/uploads');
+    if(!fs.existsSync(uploadDir)){
+        fs.mkdirSync(uploadDir);
+    }
+
+    const fileName = `${Date.now()}.png`;
+
+    const relativePath = `/uploads/${fileName}`;
+
+    const filePath = path.join(uploadDir,fileName);
+    fs.writeFileSync(filePath,binaryImage);
+
+    try{
+        await model.create({
+            title:title,
+            actor:actor,
+            director:director,
+            image: relativePath
+        });
         console.log("Document inserted successfully");
         res.status(201).send("success");
-    })
-    .catch((error)=>{
-        console.log("Document insertion failed");
-        res.status(400).send("failed");
-    })
+    }  catch (error){
+        console.log("Document insertion failed: ",error);
+        res.status(400).send("Failed to insert document");
+    }
+
+   
 
 });
 
 app.get('/getFilms',async(req,res)=>{
-    const datas = await model.find();
-    res.status(201).send("success");
+    try{
+        const movies = await model.find();
+        res.status(200).json(movies);
+    }catch (error) {
+        console.log('Error fetching movies:',error);
+    }
+   
    });
 
  async function connect() {
